f8912dd5705a3dae3f9a9b2791e38538b2101df8
sjfox <sjfcodes@gmail.com>
2023-12-21 17:00:22 -0800

    default to open ip when not on local network

--- src/utilities/ipAddress.js
+++ src/utilities/ipAddress.js
@@ -1,4 +1,4 @@
 import { networkInterfaces } from 'os';
 
 const nets = networkInterfaces();
-export const ipAddress = nets.wlan0[0].address
+export const ipAddress = nets.wlan0?.[0]?.address || '0.0.0.0';
