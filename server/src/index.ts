16af4b1963c8b5809514c7467690e7ecf490fc77
sjfox <sjfcodes@gmail.com>
2023-12-21 19:57:26 -0800

    re-install raspi-gpio

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { createServer } from "node:http";
-// import { DigitalOutput } from 'raspi-gpio';
+import { DigitalOutput } from 'raspi-gpio';
 import { Server, Socket } from "socket.io";
 import { LedState } from "./types";
 import { CHANNEL_LED_PIN_STATE, DEFAULT_LED_PIN_STATE } from "./utils/constant";
@@ -9,17 +9,17 @@ import { ipAddress } from "./utils/ipAddress";
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
-// const gpio4 = new DigitalOutput('GPIO4');
+const gpio4 = new DigitalOutput('GPIO4');
 
 const { PORT = 3000 } = process.env;
 let ledPinState = DEFAULT_LED_PIN_STATE;
 
-const turnOff = (pin: unknown) => {
-  // pin.write(0);
+const turnOff = (pin: DigitalOutput) => {
+  pin.write(0);
 };
 
-const turnOn = (pin: unknown) => {
-  // pin.write(1);
+const turnOn = (pin: DigitalOutput) => {
+  pin.write(1);
 };
 
 // user disconnect
@@ -39,7 +39,7 @@ const onConnect = (socket: Socket) => {
 
 // user updates pin state
 const onUpdatePinState = (newLedState: LedState) => {
-  // (newLedState.isOn ? turnOn : turnOff)(gpio4);
+  (newLedState.isOn ? turnOn : turnOff)(gpio4);
   ledPinState.isOn = newLedState.isOn;
   // send new state to all users (including sender)
   io.emit(CHANNEL_LED_PIN_STATE, ledPinState);
@@ -50,14 +50,14 @@ io.on("connection", onConnect);
 
 app.get("/", (_, res) => res.status(200).send("raspi-home-server"));
 server.listen(PORT, () => {
-  // turnOff(gpio4);
+  turnOff(gpio4);
   console.log(`Server running at http://${ipAddress}:${PORT}.`);
 });
 
 ["SIGINT", "SIGTERM", "SIGQUIT"].forEach((signal) =>
   process.on(signal, () => {
-    // turnOff(gpio4);
-    // gpio4.destroy();
+    turnOff(gpio4);
+    gpio4.destroy();
     process.exit();
   })
 );
