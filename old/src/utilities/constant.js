0b3ecc8a113e792acbbc0c710a9e55612f8edad8
sjfox <sjfcodes@gmail.com>
2023-12-21 20:32:45 -0800

    delete old project files

--- old/src/utilities/constant.js
+++
@@ -1,2 +0,0 @@
-export const CHANNEL_LED_PIN_STATE = 'led-pin-state'
-export const DEFAULT_LED_PIN_STATE = { isOn: false }
--- old/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-import configuration from './vite.config.js';
-
-export default defineConfig({
-  ...configuration,
-  test: {
-    include: ['**/*.test.ts'],
-  },
-});
