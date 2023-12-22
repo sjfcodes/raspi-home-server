d6dc468dc692f55df40d896096f992345d3fb4c5
sjfox <sjfcodes@gmail.com>
2023-12-21 19:43:17 -0800

    mvp server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,63 @@
-import "dotenv/config";
-import express, { Express, Request, Response } from "express";
+import express from "express";
+import { createServer } from "node:http";
+// import { DigitalOutput } from 'raspi-gpio';
+import { Server, Socket } from "socket.io";
+import { LedState } from "./types";
+import { CHANNEL_LED_PIN_STATE, DEFAULT_LED_PIN_STATE } from "./utils/constant";
+import { ipAddress } from "./utils/ipAddress";
 
-const app: Express = express();
-const port = process.env.PORT || 3000;
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+// const gpio4 = new DigitalOutput('GPIO4');
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+const { PORT = 3000 } = process.env;
+let ledPinState = DEFAULT_LED_PIN_STATE;
+
+const turnOff = (pin: unknown) => {
+  // pin.write(0);
+};
+
+const turnOn = (pin: unknown) => {
+  // pin.write(1);
+};
+
+// user disconnect
+const onDisconnect = () => {
+  console.log("socket.on.disconnect");
+};
+
+// user connect
+const onConnect = (socket: Socket) => {
+  console.log("io.on.connection");
+  // send current state to user
+  socket.emit(CHANNEL_LED_PIN_STATE, ledPinState);
+  // listen for events from user
+  socket.on(CHANNEL_LED_PIN_STATE, onUpdatePinState);
+  socket.on("disconnect", onDisconnect);
+};
+
+// user updates pin state
+const onUpdatePinState = (newLedState: LedState) => {
+  // (newLedState.isOn ? turnOn : turnOff)(gpio4);
+  ledPinState.isOn = newLedState.isOn;
+  // send new state to all users (including sender)
+  io.emit(CHANNEL_LED_PIN_STATE, ledPinState);
+  console.log(CHANNEL_LED_PIN_STATE, ledPinState);
+};
+
+io.on("connection", onConnect);
+
+app.get("/", (_, res) => res.status(200).send("raspi-home-server"));
+server.listen(PORT, () => {
+  // turnOff(gpio4);
+  console.log(`Server running at http://${ipAddress}:${PORT}.`);
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+["SIGINT", "SIGTERM", "SIGQUIT"].forEach((signal) =>
+  process.on(signal, () => {
+    // turnOff(gpio4);
+    // gpio4.destroy();
+    process.exit();
+  })
+);
---
+++ server/src/utils/constant.ts
@@ -0,0 +1,4 @@
+import { LedState } from "../types";
+
+export const CHANNEL_LED_PIN_STATE = "led-pin-state";
+export const DEFAULT_LED_PIN_STATE: LedState = { isOn: false };
