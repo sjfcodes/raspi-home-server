6537b6e36087db64033a69efa060e0df51cf5165
sjfox <sjfcodes@gmail.com>
2024-01-02 22:16:07 -0800

    replace temp history to calc averag temp

--- server/src/esp32/temperature.ts
+++ server/src/esp32/temperature.ts
@@ -20,6 +20,15 @@ export const setEsp32Client = (
     console.error(new Error("client.chipId must be defined"));
     return;
   }
+  
+  const maxLen = 60;
+  const temp = Math.trunc(client.tempF);
+  let tempFHistory = clientMapState[client.chipId]?.tempFHistory || [];
+  if (tempFHistory.length < maxLen) tempFHistory.unshift(temp);
+  else tempFHistory = [temp, ...tempFHistory.slice(0, maxLen - 1)];
+
+  const tempAverage =
+    tempFHistory.reduce((acc, curr) => acc + curr, 0) / tempFHistory.length;
 
   clientMapState = getSortedObject({
     ...clientMapState,
@@ -27,7 +36,8 @@ export const setEsp32Client = (
       chipId: client.chipId,
       // @ts-ignore
       chipName: THERMOSTAT[client.chipId] || client.chipName,
-      tempF: Math.trunc(client?.tempF),
+      tempF: Math.trunc(tempAverage),
+      tempFHistory,
       calibrate: client.calibrate || 0,
       updatedAt: new Date().toLocaleTimeString(),
     },
