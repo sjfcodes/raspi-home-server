ff5337ea242627f2a1f26064cf467bfba34a7d4a
sjfox <sjfcodes@gmail.com>
2024-01-06 13:12:49 -0800

    updates header logos

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,9 @@ import SocketStatus from "./SocketStatus";
 import HeaterState from "./components/HeaterState";
 import LogStream from "./components/LogStream";
 // import OverrideButtons from "./components/OverrideButtons";
+import reactLogo from './assets/react.svg';
+import espressifLogo from "./assets/espressif.svg";
 import raspberryPiLogo from "./assets/raspberry_pi.svg";
-import raspberryPiLogo from "./assets/";
 import PiTemp from "./components/PiTemp";
 import TargetTemp from "./components/TargetTemp";
 import Thermostats from "./components/Thermostats";
@@ -13,18 +14,22 @@ function App() {
   return (
     <>
       <div style={{ display: "flex", alignItems: "center" }}>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://www.raspberrypi.com/" target="_blank">
           <img src={raspberryPiLogo} className="logo" alt="Raspbery Pi logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={raspberryPiLogo} className="logo" alt="Raspbery Pi logo" />
+        <a href="https://www.espressif.com/en/products/socs/esp32" target="_blank">
+          <img src={espressifLogo} className="logo" alt="Espressif logo" />
         </a>
-        <h2>
-          <SocketStatus />
-        </h2>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo" alt="React logo" />
+        </a>
       </div>
       <hr />
       <div>
+        <h2>Server:{' '}
+          <SocketStatus />
+        </h2>
+        <br/>
         <Thermostats />
         <TargetTemp />
         <HeaterState />
