cd9f3c3e2e001029b7297f93e50d6567ecd16327
sjfox <sjfcodes@gmail.com>
2024-01-06 13:02:31 -0800

    move svg to assets folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,19 @@ import SocketStatus from "./SocketStatus";
 import HeaterState from "./components/HeaterState";
 import LogStream from "./components/LogStream";
 // import OverrideButtons from "./components/OverrideButtons";
+import raspberryPiLogo from "./assets/raspberry_pi.svg";
+import raspberryPiLogo from "./assets/";
 import PiTemp from "./components/PiTemp";
 import TargetTemp from "./components/TargetTemp";
 import Thermostats from "./components/Thermostats";
-import raspberryPiLogo from "/raspberry_pi.svg";
 
 function App() {
   return (
     <>
       <div style={{ display: "flex", alignItems: "center" }}>
+        <a href="https://react.dev" target="_blank">
+          <img src={raspberryPiLogo} className="logo" alt="Raspbery Pi logo" />
+        </a>
         <a href="https://react.dev" target="_blank">
           <img src={raspberryPiLogo} className="logo" alt="Raspbery Pi logo" />
         </a>
