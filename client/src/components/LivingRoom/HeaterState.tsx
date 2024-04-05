e0d77e940c7cddf9d554141251962e205eed10f4
sjfox <sjfcodes@gmail.com>
2024-04-05 00:58:23 -0700

    make heater state card uinform

--- client/src/components/LivingRoom/HeaterState.tsx
+++ client/src/components/LivingRoom/HeaterState.tsx
@@ -1,7 +1,18 @@
 import useHeaterSse from "../../hooks/useHeaterSse";
+import Card from "../Card";
+import JsonCode from "../JsonCode";
 
 export default function HeaterState() {
   const [state] = useHeaterSse();
 
-  return <pre>{JSON.stringify(state, null, 4)}</pre>;
+  return (
+    <Card
+      label={`House Heater Cabinet`}
+      content={
+        <div style={{ width: "100%", overflow: "scroll" }}>
+          <JsonCode code={JSON.stringify(state, null, 4)} />
+        </div>
+      }
+    />
+  );
 }
