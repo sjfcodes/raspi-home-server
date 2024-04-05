886c4d8b1d000dd5e4088f98788560ff1b61d4c9
sjfox <sjfcodes@gmail.com>
2024-04-04 23:00:14 -0700

    wip server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,18 @@
+
 import { RASP_PI } from "../../constant/constant";
 import { initHomeHeater } from "./service/esp32/heater";
 import { initHomeCron } from "./service/esp32/homeCron";
-import { initAllThermostats, setThermostatClient } from "./service/esp32/thermostat";
-import { setPiTemp } from "./service/pi/temperature";
+import { initAllThermostats } from "./service/esp32/thermostat";
+import { initPiState } from "./service/pi/temperature";
 import { app } from "./service/server";
+import { log } from "./utils/general";
 import { ipAddress } from "./utils/ipAddress";
 
 const { PORT = RASP_PI.serverPort } = process.env;
-const LOOP_MS = 1000;
-
-app.post("/api/temperature", (req, res) => {
-    if (req.body) setThermostatClient(req.body);
-    res.status(200).send({ ...req.body, serverName: "raspi-home-server" });
-});
 
 initAllThermostats();
 initHomeHeater();
 initHomeCron();
+initPiState();
 
-app.listen(PORT, () => {
-    setInterval(setPiTemp, LOOP_MS);
-    console.log(`Running server at http://${ipAddress}:${PORT}`);
-});
+app.listen(PORT, () => log('server', `start (http://${ipAddress}:${PORT})`));
