c8299faca0fe24d5e48fb53086d69f11636c2c4b
sjfox <sjfcodes@gmail.com>
2024-04-08 23:34:07 -0700

    delete demo user example

--- server/src/api/components/user/router.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { readUsers } from './controller';
-
-const userRouter = Router();
-userRouter.get('/', readUsers);
-
-export { userRouter };
--- server/src/api/components/user/store.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "./model";
-
-const user = new User(0, 'email', 'first', 'last');
-const db = [user];
-
-export async function readAll(): Promise<User[]>{
-    return db;
-}
