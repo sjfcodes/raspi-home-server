85ad40af8e6c01c7670252344ad2c9e41559698c
sjfox <sjfcodes@gmail.com>
2024-04-10 02:08:36 -0700

    adds updatedAt key to remote model

--- server/src/api/components/remote/model.ts
+++ server/src/api/components/remote/model.ts
@@ -1,13 +1,21 @@
 export class Item {
-    constructor(id: string, type: 'F' | 'C', min: number, max: number) {
+    constructor(
+        id: string,
+        type: 'F' | 'C',
+        min: number,
+        max: number,
+        updatedAt: string
+    ) {
         this.id = id;
         this.type = type;
         this.min = min;
         this.max = max;
+        this.updatedAt = updatedAt;
     }
 
     public id: string;
     public type: 'F' | 'C';
     public max: number;
     public min: number;
+    public updatedAt: string;
 }
