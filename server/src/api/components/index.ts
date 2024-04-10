a8d6ae15e466aae931ea35c0ab4f5bb2efb322ea
sjfox <sjfcodes@gmail.com>
2024-04-10 00:06:26 -0700

    planning

--- server/src/api/components/index.ts
+++ server/src/api/components/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { registerThermostatRoutes } from './thermostat/router';
-import { registerItemRoutes } from './template/router';
+import { registerItemRoutes } from './_template/router';
+import { registerHeaterRoutes } from './heater/router';
 
 export function registerApiRoutes(router: Router, prefix: string = ''): void {
     registerItemRoutes(router, `${prefix}/item`);
-    registerItemRoutes(router, `${prefix}/room`);
+    registerHeaterRoutes(router, `${prefix}/heater`);
 
     // [TODO] update thermostats to use new route
     registerThermostatRoutes(router, `${prefix}/thermostat`);
@@ -21,40 +22,40 @@ export function registerApiRoutes(router: Router, prefix: string = ''): void {
  * [NEXT]
  * --- ROUTES ----
  * HEATER 
+ *     [NOTE]: uses websocket to write heater state changes
  *     PATH: /heater
  *     GET : read heaters
  *     
- *     PATH: /heater/:id
- *     GET : read one heater
+ *     [TODO] PATH: /heater/:id
+ *            GET : read one heater
  *  
- *     NOTE: is websocket connection to write heater state change
  *     [TODO]: convert heater chip to use
- *             GET : read sse status stream
- *             POST: write heater state
+ *             GET  : read sse status stream
+ *             PATCH: write heater state
  *
  * CONTROLLER
  *     PATH: /controller
- *     POST: write min, max, override to store
+ *     PUT: write min, max, override to store
  *     GET : read controllers
  *
- *     PATH: /controller/:id
- *     GET : read one controller
+ *     [TODO] PATH: /controller/:id
+ *            GET : read one controller
  *
  * THERMOSTAT
  *     PATH: /thermostat
- *     POST: write temperature to store
+ *     PUT: write temperature to store
  *     GET : read thermostats
  *
- *     PATH: /thermostat/:id
- *     GET : read one thermostat
+ *     [TODO] PATH: /thermostat/:id
+ *            GET : read one thermostat
  *
  * ZONE
  *     PATH: /zone
- *     POST: write zone to store
+ *     PUT: write zone to store
  *     GET : read zones
  *
- *     PATH: /zone/:id
- *     GET : read one zone
+ *     [TODO] PATH: /zone/:id
+ *            GET : read one zone
  *
  * SYSTEM
  *     PATH: /system
