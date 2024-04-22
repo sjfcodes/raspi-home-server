db9746a57f18d6d544777ee7f556f2b43d2b8348
sjfox <sjfcodes@gmail.com>
2024-04-21 19:06:00 -0700

    use icons for target temp buttons

--- client/src/components/Thermostat/Thermostat.TemperatureButtons/Thermostat.TemperatureButtons.tsx
+++ client/src/components/Thermostat/Thermostat.TemperatureButtons/Thermostat.TemperatureButtons.tsx
@@ -1,4 +1,5 @@
 import { useAtom } from 'jotai';
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import { Zone } from '../../../../../types/main';
 import { heaterMapAtom } from '../../../store/heaterMap.atom';
 import ThermostatControl from '../Thermostat.Control';
@@ -9,21 +10,23 @@ type Props = { zone: Zone };
 export default function ThermostatTemperatureButtons({ zone }: Props) {
     const [heaterMap] = useAtom(heaterMapAtom);
     const heater = heaterMap?.[zone.heaterId];
+
     if (!heater) return null;
+    const iconSize = 50;
 
     return (
         <div className="item-card-100-x-25 thermostat-temperature-buttons">
             <ThermostatControl
-                className="item-card-50-x-25 thermostat-control-cooler"
+                className="item-card-50-x-25 thermostat-control-cooler text-large"
                 onClick={() => thermostatControlDown(zone.thermostatId)}
             >
-                cooler
+                <IoIosArrowDown size={iconSize} />
             </ThermostatControl>
             <ThermostatControl
-                className="item-card-50-x-25 thermostat-control-warmer"
+                className="item-card-50-x-25 thermostat-control-warmer text-large"
                 onClick={() => thermostatControlUp(zone.thermostatId)}
             >
-                warmer
+                <IoIosArrowUp size={iconSize} />
             </ThermostatControl>
         </div>
     );
