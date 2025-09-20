c9643bb68033d6700b03e41a2a2547f6f20ad20a
sjfox <sjfcodes@gmail.com>
2025-09-20 13:01:39 -0700

    remove template route registration

--- server/src/api/components/index.ts
+++ server/src/api/components/index.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express';
 import { registerThermometerRoutes } from './thermometer/router';
-import { registerItemRoutes } from './_template/router';
 import { registerHeaterRoutes } from './heater/router';
 import { registerThermostatRoutes } from './thermostat/router';
 import { registerZoneRoutes } from './zone/router';
 import { registerSystemRoutes } from './system/router';
 
 export function registerApiRoutes(router: Router, prefix: string = ''): void {
-    registerItemRoutes(router, `${prefix}/item`);
     registerHeaterRoutes(router, `${prefix}/heater`);
     registerThermostatRoutes(router, `${prefix}/thermostat`);
     registerZoneRoutes(router, `${prefix}/zone`);
